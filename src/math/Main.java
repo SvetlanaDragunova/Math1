b11e368b74dd60b566a044f8835f4b854e741b22
SvetlanaDragunova <dragunovasa@yandex.ru>
2016-09-19 23:12:08 +0300

    A bit changed to become more readable

--- src/math/Main.java
+++ src/math/Main.java
@@ -25,12 +25,11 @@ public class Main {
         System.out.println(deltaparab(20,0.,0.78539816339));
 
         System.out.println("Решение определенного интеграла №4 методом парабол");
-        System.out.println("Определяем количество итераций для точности 0.00001 с помощью формулы погрешности");
-        System.out.println(getn(0.00001,0.8,1.2));
-        System.out.println(parabf2(6,0.8,1.2));
-        System.out.println("Погрешность");
-        System.out.println(deltaparab(6,0.8,1.2));
-        System.out.println(Runge(0.00001,0.8,1.2));
+        System.out.println("Определяем количество итераций для точности 0.00001 с помощью формулы погрешности: "+getn(0.00001,0.8,1.2));
+        System.out.println("Значение интеграла с заданным количеством итераций "+parabf2(6,0.8,1.2));
+        System.out.println();
+        System.out.println("Решаем с точностью 0.00001 с помощью метода Рунге");
+        System.out.println("Значение интеграла с заданным количеством итераций "+Runge(0.00001,0.8,1.2));
     }
 
     public static double f1 (double x){
@@ -85,6 +84,11 @@ public class Main {
         return res;
     }
 
+    public static double deltaparab (int n, double a, double b){
+
+        return 722.124*(b-a)*(b-a)*(b-a)*(b-a)*(b-a)/(180*n*n*n*n);
+    }
+
     public static double parabf2(int n, double a, double b){
         double h = (b-a)/n;
 
@@ -106,15 +110,10 @@ public class Main {
         return res;
     }
 
-    public static double deltaparab (int n, double a, double b){
-
-        return 722.124*(b-a)*(b-a)*(b-a)*(b-a)*(b-a)/(180*n*n*n*n);
-    }
 
     public static int getn(double eps, double a, double b){
 
         double res = Math.pow(144.604*(b-a)*(b-a)*(b-a)*(b-a)*(b-a)/(180*eps),0.25);
-        System.out.println(res);
         return (int)Math.ceil(res);
     }
 
@@ -128,12 +127,17 @@ public class Main {
         int n = 1;
         double res1 = parabf2(n,a,b);
         double res2 = parabf2(2*n,a,b);
-        while (Math.abs(res1-res2)>keps){
+        double delta = Math.abs(res1-res2);
+        System.out.println("При "+n+"-ой итерации интеграл равен "+res1);
+        System.out.println("При "+2*n+"-ой итерации интеграл равен "+res2+", дельта равна "+delta);
+        while (delta>keps){
             n = n*2;
             res1 = res2;
             res2 = parabf2(2*n,a,b);
+            delta = Math.abs(res1-res2);
+            System.out.println("При "+2*n+"-ой итерации интеграл равен "+res2+", дельта равна "+delta);
         }
-
+        System.out.println("Так как дельта меньше, чем k*eps, то останавливаем алгоритм");
         System.out.println("Количество итераций по методу Рунге равно "+ 2*n);
         return res2;
     }
